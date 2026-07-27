e8e252e93b46ec5bc9eb777cc1a8a478891faff5
Matheus Carneiro <matheus.leite.carneiro@gmail.com>
2026-07-27 17:04:14 -0300

    feat: add method to fetch the event with the ticketType

--- src/main/java/com/mlcdev/soldout/event/TicketTypeRepository.java
+++ src/main/java/com/mlcdev/soldout/event/TicketTypeRepository.java
@@ -1,11 +1,17 @@
 package com.mlcdev.soldout.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 
 @Repository
 interface TicketTypeRepository extends JpaRepository<TicketType, UUID> {
+
+    @Query("SELECT tt FROM TicketType tt JOIN FETCH tt.event WHERE tt.id = :uuid")
+    Optional<TicketType> findByIdWithEvent(@Param("uuid") UUID uuid);
 }

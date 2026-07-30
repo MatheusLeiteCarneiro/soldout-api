a451e6bd2b35f9cc63e7103c60841ad4dd591c53
Matheus Carneiro <matheus.leite.carneiro@gmail.com>
2026-07-30 15:50:49 -0300

    feat: add query to find all ticket types by event id

--- src/main/java/com/mlcdev/soldout/event/TicketTypeRepository.java
+++ src/main/java/com/mlcdev/soldout/event/TicketTypeRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,6 +17,9 @@ interface TicketTypeRepository extends JpaRepository<TicketType, UUID> {
 
     @Query("SELECT tt FROM TicketType tt JOIN FETCH tt.event WHERE tt.id = :uuid")
     Optional<TicketType> findByIdWithEvent(@Param("uuid") UUID uuid);
+
+    @Query("SELECT t FROM TicketType t WHERE t.event.id = :eventId")
+    List<TicketType> findAllByEventId(@Param("eventId") UUID eventId);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT tt FROM TicketType tt JOIN FETCH tt.event WHERE tt.id = :uuid")

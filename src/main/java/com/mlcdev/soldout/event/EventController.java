d6ad1c5340cb5cd4a2ce758927abc34fb4cebf9b
Matheus Carneiro <matheus.leite.carneiro@gmail.com>
2026-07-30 23:20:46 -0300

    fix: add default sort to keep event pagination stable

--- src/main/java/com/mlcdev/soldout/event/EventController.java
+++ src/main/java/com/mlcdev/soldout/event/EventController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -29,7 +30,7 @@ public class EventController {
     private final EventService service;
 
     @GetMapping
-    public ResponseEntity<Page<EventSummaryDTO>> findAllAvailable(Pageable pageable){
+    public ResponseEntity<Page<EventSummaryDTO>> findAllAvailable(@PageableDefault(sort = {"startsAt", "id"}) Pageable pageable){
         return ResponseEntity.ok(service.findAllAvailable(pageable));
     }
 

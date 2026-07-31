c00e8e68b2b92a9b1c04c1430fa53034593ebbe1
Matheus Carneiro <matheus.leite.carneiro@gmail.com>
2026-07-31 10:47:42 -0300

    feat: add global exception handler

---
+++ src/main/java/com/mlcdev/soldout/shared/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.mlcdev.soldout.shared.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ResponseEntity<ApiErrorDTO> handleValidation(MethodArgumentNotValidException e, HttpServletRequest request){
+        int status = HttpStatus.BAD_REQUEST.value();
+        List<FieldErrorDTO> fieldErrors = new ArrayList<>();
+        e.getBindingResult().getFieldErrors().forEach(f -> fieldErrors.add(new FieldErrorDTO(f.getField(), f.getDefaultMessage())));
+        ApiErrorDTO error = new ApiErrorDTO(status, "data validation not passed", request.getRequestURI(), fieldErrors);
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    private ResponseEntity<ApiErrorDTO> handleResourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+        int status = HttpStatus.NOT_FOUND.value();
+        ApiErrorDTO error = new ApiErrorDTO(status, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+}
